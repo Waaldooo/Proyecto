9120346cd365a61a51a38833f1e0abcef311764f
Waaldooo <ca443177@uaeh.edu.mx>
2026-03-31 21:26:51 -0600

    Proyecto

--- src/pck_Proyecto/MenuRenta.java
+++ src/pck_Proyecto/MenuRenta.java
@@ -3,5 +3,25 @@ package pck_Proyecto;
 public class MenuRenta {
    public static void main(String[] args){
        
+       
+       String menu = " - - - MENU RENTA DE AUTOS Y MOTOS - - -\n\n" +
+               "1) Alta de automóvil\n" +
+               "2) Alta de una motocicleta\n" +
+               "3) Alta de un cliente\n" +
+               "4) Alta de una renta\n" +
+               "5) Listar automóviles\n" +
+               "6) Listar motocicletas\n" +
+               "7) Listar clientes\n" +
+               "8) Listar rentas\n" +
+               "9) Ver detalles de un automóvil\n" +
+               "10) Ver detalles de una motocicleta\n" +
+               "11) Ver detalles de un cliente\n" +
+               "12) Ver detalles de una renta\n" +
+               "13) Eliminar un automóvil\n" + 
+               "14) Eliminar una motocicleta\n" +
+               "15) Eliminar un cliente\n" +
+               "16) Eliminar una renta\n" +
+               "17) Salir\n" +
+               "\nElija una opcion:";
    } 
 }
