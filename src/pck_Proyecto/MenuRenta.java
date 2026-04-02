e7b0cc4ed67028aadf9dcd478f8fa283852350f5
Waaldooo <ca443177@uaeh.edu.mx>
2026-04-01 23:17:28 -0600

    Proyecto

--- src/pck_Proyecto/MenuRenta.java
+++ src/pck_Proyecto/MenuRenta.java
@@ -48,7 +48,7 @@ public class MenuRenta {
                    do{
                        idVehiculo = -1;
                        try{
-                            idVehiculo = Integer.parseInt(JOptionPane.showInputDialog(null, "Id del Automovil", "Alta de un Automóvil", 3));
+                            idVehiculo = Integer.parseInt(JOptionPane.showInputDialog(null, "Id del Automovil:", "Alta de un Automóvil", 3));
                             if(idVehiculo <= 0){
                                 JOptionPane.showMessageDialog(null, "El Id del Automóvil debe ser positivo", "Dato Erroneo", 2);
                             } else if(existeVehiculo(vehiculo, idVehiculo, contV)){
@@ -60,27 +60,151 @@ public class MenuRenta {
                        }
                    } while (idVehiculo <= 0);
                    
-                    modelo = JOptionPane.showInputDialog(null, "Modelo:", "Alta de un Automóvil", 3);
-                    
-                    marca = JOptionPane.showInputDialog(null, "Marca:", "Alta de un Automóvil", 3);
-                    
-                    anio = Integer.parseInt(JOptionPane.showInputDialog(null, "Anio:", "Alta de un Automóvil"));
-                    
-                    color = JOptionPane.showInputDialog(null, "Color:", "Alta de un Automóvil", 3);
-                    
-                    tipo = JOptionPane.showInputDialog(null, "Tipo:", "Alta de un Automóvil", 3);
-                    
-                    transmision = JOptionPane.showInputDialog(null, "Transmisión:", "Alta de un Automóvil", 3);
-                    
-                    noPuertas = Integer.parseInt(JOptionPane.showInputDialog(null, "No. de puertas:", "Alta de un Automóvil", 3));
+                   do{
+                        modelo = JOptionPane.showInputDialog(null, "Modelo:", "Alta de un Automóvil", 3);
+                        if(modelo.isBlank()){
+                            JOptionPane.showMessageDialog(null, "El modelo es algo requerido.", "Dato erroneo", 2);
+                        }
+                   } while(modelo.isBlank());
+                   
+                   do{
+                        marca = JOptionPane.showInputDialog(null, "Marca:", "Alta de un Automóvil", 3);
+                        if(marca.isBlank()){
+                            JOptionPane.showMessageDialog(null, "La marca es algo requerido.", "Dato Erroneo", 2);
+                        }
+                   } while(marca.isBlank());
+                   
+                   do{
+                       anio = 0;
+                       try{
+                            anio = Integer.parseInt(JOptionPane.showInputDialog(null, "Año:", "Alta de un Automóvil"));
+                            if(anio < 1900 || anio > 2027){
+                                JOptionPane.showMessageDialog(null, "El año debe se estar entre 1900 y 2027", "Dato erroneo", 2);
+                            }
+                       }catch(NumberFormatException e){
+                           JOptionPane.showMessageDialog(null, "El año debe ser numerico", "Dato erroneo", 2);
+                       }
+                   } while (anio < 1900 || anio > 2027);
+                   
+                   do{
+                        color = JOptionPane.showInputDialog(null, "Color:", "Alta de un Automóvil", 3);
+                        if(color.isBlank()){
+                            JOptionPane.showMessageDialog(null, "El color es algo requerido", "Dato erroneo", 2);
+                        }
+                   } while(color.isBlank());
+                   
+                   do{
+                        tipo = JOptionPane.showInputDialog(null, "Tipo:", "Alta de un Automóvil", 3);
+                        if(tipo.isBlank()){
+                            JOptionPane.showMessageDialog(null, "El tipo es algo requerido", "Dato erroneo", 2);
+                        }
+                   } while(tipo.isBlank());
+                   
+                   do{
+                        transmision = JOptionPane.showInputDialog(null, "Transmisión:", "Alta de un Automóvil", 3);
+                        if(transmision.isBlank()){
+                            JOptionPane.showMessageDialog(null, "La transmision es algo requerido", "Dato erroneo", 2);
+                        }
+                   } while(transmision.isBlank());
+                   
+                   do{
+                       noPuertas = 0;
+                       try{
+                            noPuertas = Integer.parseInt(JOptionPane.showInputDialog(null, "No. de puertas:", "Alta de un Automóvil", 3));
+                            if(noPuertas < 2 || noPuertas > 4){
+                                JOptionPane.showMessageDialog(null, "El no. de puertas debe ser entre 2 y 4", "Dato erroneo", 2);
+                            }
+                       }catch(NumberFormatException e){
+                           JOptionPane.showMessageDialog(null, "El no. de puertas de ser numerico", "Dato erroneo", 2);
+                       }
+                   } while(noPuertas < 2 || noPuertas > 4);
                     break;
                     
                 case 2:
                     // Alta de una motocicleta
+                    do{
+                       idVehiculo = -1;
+                       try{
+                            idVehiculo = Integer.parseInt(JOptionPane.showInputDialog(null, "Id del Automovil:", "Alta de un Automóvil", 3));
+                            if(idVehiculo <= 0){
+                                JOptionPane.showMessageDialog(null, "El Id del Automóvil debe ser positivo", "Dato Erroneo", 2);
+                            } else if(existeVehiculo(vehiculo, idVehiculo, contV)){
+                                JOptionPane.showMessageDialog(null, "Id de vehiculo ya existente", "ERROR", 2);
+                                idVehiculo = -1;
+                            }
+                       } catch (NumberFormatException e){
+                           JOptionPane.showMessageDialog(null, "El id del automóvil debe ser numerico", "Dato erroneo", 2);
+                       }
+                   } while (idVehiculo <= 0);
+                   
+                   do{
+                        modelo = JOptionPane.showInputDialog(null, "Modelo:", "Alta de un Automóvil", 3);
+                        if(modelo.isBlank()){
+                            JOptionPane.showMessageDialog(null, "El modelo es algo requerido.", "Dato erroneo", 2);
+                        }
+                   } while(modelo.isBlank());
+                   
+                   do{
+                        marca = JOptionPane.showInputDialog(null, "Marca:", "Alta de un Automóvil", 3);
+                        if(marca.isBlank()){
+                            JOptionPane.showMessageDialog(null, "La marca es algo requerido.", "Dato Erroneo", 2);
+                        }
+                   } while(marca.isBlank());
+                   
+                   do{
+                       anio = 0;
+                       try{
+                            anio = Integer.parseInt(JOptionPane.showInputDialog(null, "Año:", "Alta de un Automóvil"));
+                            if(anio < 1900 || anio > 2027){
+                                JOptionPane.showMessageDialog(null, "El año debe se estar entre 1900 y 2027", "Dato erroneo", 2);
+                            }
+                       }catch(NumberFormatException e){
+                           JOptionPane.showMessageDialog(null, "El año debe ser numerico", "Dato erroneo", 2);
+                       }
+                   } while (anio < 1900 || anio > 2027);
+                   
+                   do{
+                        color = JOptionPane.showInputDialog(null, "Color:", "Alta de un Automóvil", 3);
+                        if(color.isBlank()){
+                            JOptionPane.showMessageDialog(null, "El color es algo requerido", "Dato erroneo", 2);
+                        }
+                   } while(color.isBlank());
+                   
+                   do{
+                        tipo = JOptionPane.showInputDialog(null, "Tipo:", "Alta de un Automóvil", 3);
+                        if(tipo.isBlank()){
+                            JOptionPane.showMessageDialog(null, "El tipo es algo requerido", "Dato erroneo", 2);
+                        }
+                   } while(tipo.isBlank());
+                   
+                   do{
+                        noVelocidades = 0;
+                        try{
+                            noVelocidades = Integer.parseInt(JOptionPane.showInputDialog(null, "No. de velocidades:", "Alta de una motocicleta", 3));
+                            if(noVelocidades < 4 || noVelocidades > 7){
+                                JOptionPane.showMessageDialog(null, "El no. de velocidades debe estar entre 4 y 7");
+                            }
+                        } catch(NumberFormatException e){
+                            JOptionPane.showMessageDialog(null, "El no. de velocidades debe ser numerico", "Dato erroneo", 2);
+                        }
+                   } while(noVelocidades < 4 || noVelocidades > 7);
+                   
+                   do{
+                       noLlantas = 0;
+                       try{
+                            noLlantas = Integer.parseInt(JOptionPane.showInputDialog(null, "No. de llantas:", "Alta de una motocicleta", 3));
+                            if(noLlantas != 2){
+                                JOptionPane.showMessageDialog(null, "El no. de llantas debe ser igual a 2 para que sea moto", "Dato erroneo", 2);
+                            }
+                       } catch(NumberFormatException e){
+                           JOptionPane.showMessageDialog(null, "El no. de llantas debe ser numerico", "Dato erroneo", 2);
+                       }
+                   } while(noLlantas != 2);
                     break;
                     
                 case 3:
                     // Alta de un cliente
+                   
                     break;
                     
                 case 4:
@@ -146,8 +270,10 @@ public class MenuRenta {
    public static boolean existeVehiculo(Vehiculo[] E, int idVehiculo, int cont){
        boolean existe = false;
        for(int i = 0; i < cont; i++){
+           if(E[i].getIdVehiculo() == idVehiculo){
            existe = true;
            break;
+           }
        }
        return existe;
    }
